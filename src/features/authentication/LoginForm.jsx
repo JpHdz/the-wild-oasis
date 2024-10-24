d6d5a5a40bfacfae4f9183a408f490cf90554f40
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-10-24 17:33:07 -0600

    Added Login Default Input

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@demo.com");
+  const [password, setPassword] = useState("demo");
 
   const { login, isLoading } = useLogin();
 
